3ca7fd61b9d5336b44ddb93a6521ee9e01b416fe
Lazaro1 <lazarolimasilva2@gmail.com>
2021-04-05 15:44:58 -0300

    object

--- src/App.js
+++ src/App.js
@@ -2,24 +2,45 @@ import React from 'react';
 import Produtos from './components/Produtos/produtos';
 
 const App = () => {
-  const produtos = {
+//   const produtos = {
+//     "id": 1,
+//     "title": "Hamburguer Artesanal Cheddar Burger",
+//     "price": 17.99,
+//     "imageUrl": "https://www.sabornamesa.com.br/media/k2/items/cache/b9ad772005653afce4d4bd46c2efe842_L.jpg",
+//     "url": "https://api.whatsapp.com/send?phone=5538997351977&text=Ol%C3%A1!%20Gostaria%20de%20realizar%20o%20meu%20pedido.%20",
+//       "comments": [
+//         {
+//           "id": 1,
+//           "comment": "Aproveite essa oferta!"
+//         }
+//       ]
+// };
+
+const products = [
+  {
     "id": 1,
     "title": "Hamburguer Artesanal Cheddar Burger",
     "price": 17.99,
     "imageUrl": "https://www.sabornamesa.com.br/media/k2/items/cache/b9ad772005653afce4d4bd46c2efe842_L.jpg",
     "url": "https://api.whatsapp.com/send?phone=5538997351977&text=Ol%C3%A1!%20Gostaria%20de%20realizar%20o%20meu%20pedido.%20",
-      "comments": [
-        {
-          "id": 1,
-          "comment": "Aproveite essa oferta!"
-        }
-      ]
-};
+    "comments": "Aproveite essa oferta!",
+  },
+  {
+    "id": 1,
+    "title": "Hamburguer Artesanal Cheddar Burger",
+    "price": 17.99,
+    "imageUrl": "https://www.sabornamesa.com.br/media/k2/items/cache/b9ad772005653afce4d4bd46c2efe842_L.jpg",
+    "url": "https://api.whatsapp.com/send?phone=5538997351977&text=Ol%C3%A1!%20Gostaria%20de%20realizar%20o%20meu%20pedido.%20",
+    "comments": "Aproveite essa oferta!",
+  }
+]
  
 return (
     <div className="App" style={{maxWidth: 800, margin: '30px auto',}}>
-      <Produtos produtos={produtos} />
-       </div>
+      {products.map((product) => (
+        <Produtos title={product.title} price={product.price} imageUrl={product.imageUrl} url={product.url} comment={product.comments} />
+      ))}
+    </div>
   );
 }
 
--- src/components/Produtos/produtos.js
+++ src/components/Produtos/produtos.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import './prod.css';
 
-const Produtos = ({ produtos }) => (
+
+const Produtos = ({ title, price, imageUrl, url, comment }) => (
     <div className="produto">
-        <img src={produtos.imageUrl} className="produto__image" />
+        <img src={imageUrl} className="produto__image" />
         <div className="produto__info">
-            <h1 className="produto__title">{produtos.title}</h1>
+            <h1 className="produto__title">{title}</h1>
             <footer className="produto__footer">
-                {produtos.comments.length > 0 && (
-                <div className="produto__comment">"{produtos.comments[0].comment}"</div>
-                )}
-            <a href={produtos.url} target="_blank" className="produto__link">Fazer Pedido</a>
+            <div className="produto__comment">"{comment}"</div>
+            <a href={url} target="_blank" className="produto__link">Fazer Pedido</a>
             </footer>
-            <span className="produto__price">R$ {produtos.price}</span>
+            <span className="produto__price">R$ {price}</span>
         </div>
     </div>
 );
