28364336e75482228ca01353d1c42445827492a6
Lazaro1 <lazarolimasilva2@gmail.com>
2021-04-06 08:08:25 -0300

    api-wpp

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -10,7 +10,6 @@ function Home() {
             "title": "Hamburguer Artesanal Cheddar Burger",
             "price": 17.99,
             "imageUrl": "https://www.sabornamesa.com.br/media/k2/items/cache/b9ad772005653afce4d4bd46c2efe842_L.jpg",
-            "url": `https://api.whatsapp.com/send?phone=5538997351977&text=${products[0].price}`,
             "comments": "Aproveite essa oferta!",
         },
         {
@@ -18,16 +17,16 @@ function Home() {
             "title": "Hamburguer Artesanal Cheddar Burger",
             "price": 17.99,
             "imageUrl": "https://www.sabornamesa.com.br/media/k2/items/cache/b9ad772005653afce4d4bd46c2efe842_L.jpg",
-            "url": "https://api.whatsapp.com/send?phone=5538997351977&text=Ol%C3%A1!%20Gostaria%20de%20realizar%20o%20meu%20pedido.%20",
             "comments": "Aproveite essa oferta!",
         }
         ]
+
 
     return(
         <div className="home-container">
             <h1>Cardápio</h1>
             {products.map((product) => (
-                <Produtos title={product.title} price={product.price} imageUrl={product.imageUrl} url={product.url} comment={product.comments} />
+                <Produtos title={product.title} price={product.price} imageUrl={product.imageUrl} comment={product.comments} />
             ))}
         </div>
     )
--- src/components/Produtos/produtos.js
+++ src/components/Produtos/produtos.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './prod.css';
 
-function teste(price) {
-    console.log(price)
-}
 
-const Produtos = ({ title, price, imageUrl, url, comment }) => (   
+function Produtos({ title, price, imageUrl, comment  }) {
+
+    var  WthUrl = `http://api.whatsapp.com/send?phone=5538997351977&text= Produto: ${title}, Preço: ${price}`
+
+    return (
     <div className="produto">
         <img src={imageUrl} className="produto__image" />
         <div className="produto__info">
             <h1 className="produto__title">{title}</h1>
             <footer className="produto__footer">
-            <div className="produto__comment">"{comment}"</div>``
-            <a href="https://api.whatsapp.com/send?phone=5538997351977&text=oi" target="_blank" className="produto__link">Fazer Pedido</a>
+            <div className="produto__comment">"{comment}"</div>
+            <a href={WthUrl} target="_blank" className="produto__link">Fazer Pedido</a>
             </footer>
             <span className="produto__price">R$ {price}</span>
-            <button onClick={teste}>teste</button>
         </div>
     </div>
-);
+    )
+}
 
 export default Produtos;
