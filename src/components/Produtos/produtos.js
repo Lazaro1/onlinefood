14ae0d270a0f4e845e0c2643a9071db1e262263a
LAZARO <lazarolimasilva2@gmail.com>
2021-04-06 06:48:54 -0300

    home

--- src/components/Produtos/produtos.js
+++ src/components/Produtos/produtos.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './prod.css';
 
+function teste(price) {
+    console.log(price)
+}
 
-const Produtos = ({ title, price, imageUrl, url, comment }) => (
+const Produtos = ({ title, price, imageUrl, url, comment }) => (   
     <div className="produto">
         <img src={imageUrl} className="produto__image" />
         <div className="produto__info">
             <h1 className="produto__title">{title}</h1>
             <footer className="produto__footer">
-            <div className="produto__comment">"{comment}"</div>
-            <a href={url} target="_blank" className="produto__link">Fazer Pedido</a>
+            <div className="produto__comment">"{comment}"</div>``
+            <a href="https://api.whatsapp.com/send?phone=5538997351977&text=oi" target="_blank" className="produto__link">Fazer Pedido</a>
             </footer>
             <span className="produto__price">R$ {price}</span>
+            <button onClick={teste}>teste</button>
         </div>
     </div>
 );
